8da5edcb3b15acd7598e5d728b4e3359bc6c3dbe
Everett Johnson <root@Everetts-MacBook-Pro.local>
2020-10-11 19:53:23 -0600

    Cleaned up main

--- source/main.cpp
+++ source/main.cpp
@@ -7,18 +7,9 @@
 
 int main() {
     
-    /*
-    ChatServer* SERVER = SERVER->getInstance();
-
-    while(1)
-        SERVER->ACCEPT_CLIENTS();
-    */
 
     packets::NTP* testPacket = (packets::NTP*)malloc(sizeof(testPacket));
     *((char *)testPacket) = 0x1b;
-    //std::cout << sizeof(*testPacket) << std::endl;
-    //testPacket->li_vn_mode = SET_LI_VI_MODE(0,3,3);
-    //testPacket->stratum    = 3;
     ChatClient* UDP_TEST = new ChatClient(123); 
     UDP_TEST->sendDatagram( (char *)(testPacket) );
 
